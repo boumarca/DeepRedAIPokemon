bf6d0671f602ed1e60bb71fc7fbc5634245f0a98
Marc-André Boucher <ma.boucher@illogika.com>
2019-08-30 12:30:09 -0400

    Update UI in battle state

--- DeepRedPokemonAI/Assets/Scripts/Showdown/ClientBattleState.cs
+++ DeepRedPokemonAI/Assets/Scripts/Showdown/ClientBattleState.cs
@@ -1,12 +1,16 @@
 ﻿using DeepRedAI.Parser;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace DeepRedAI.Showdown
 {
 	public class ClientBattleState : ClientState
-	{	
+	{
+		[SerializeField]
+		Text _roomIdText = default;
 		public override void ReceiveMessage(ServerMessage message)
 		{
-			
+			_roomIdText.text = _context.RoomId;
 		}
 	}
 }
